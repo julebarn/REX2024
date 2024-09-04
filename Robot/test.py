d5aefe1983656962b7b35a4cd0e0f69b3e8880bd
ArloUno <arlo@robot.com>
2024-09-04 11:25:25 +0200

    init

---
+++ Robot/test.py
@@ -0,0 +1,36 @@
+from robot import Robot 
+from time import sleep
+
+
+lspeed = 100
+rspeed = 107
+
+lturn = 60
+rturn = 60
+
+arlo = Robot()
+arlo.reset_encoder_counts()
+def t90(arlo):
+    arlo.go_diff(lturn,rturn,1,0)
+    sleep(0.8281080)
+    arlo.stop()
+    sleep(2)
+
+def line(arlo):
+    arlo.go_diff(lspeed,rspeed,1,1)
+    sleep(3)
+    arlo.stop()
+
+def sq(arlo):
+    line(arlo)
+    t90(arlo)
+    line(arlo)
+    t90(arlo)
+    line(arlo)
+    t90(arlo)
+    line(arlo)
+    t90(arlo)
+
+for i in range(4):
+    t90(arlo)
+
