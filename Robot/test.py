4ace0950c0291a35e71da64e32d41480d181419c
ArloUno <pi@arlouno>
2024-09-04 14:00:48 +0200

    test.py

--- Robot/test.py
+++ Robot/test.py
@@ -8,29 +8,31 @@ rspeed = 107
 lturn = 60
 rturn = 60
 
+meter = 1.42
 arlo = Robot()
 arlo.reset_encoder_counts()
 def t90(arlo):
     arlo.go_diff(lturn,rturn,1,0)
-    sleep(0.783)
+    sleep(0.7808)
     arlo.stop()
     sleep(2)
 
-def line(arlo):
+def line(arlo,dist):
     arlo.go_diff(lspeed,rspeed,1,1)
-    sleep(3)
+    sleep(dist)
     arlo.stop()
+    sleep(1)
 
-def sq(arlo):
-    line(arlo)
+def sq(arlo,dist):
+    line(arlo,dist)
     t90(arlo)
-    line(arlo)
+    line(arlo,dist)
     t90(arlo)
-    line(arlo)
+    line(arlo,dist)
     t90(arlo)
-    line(arlo)
+    line(arlo,dist)
     t90(arlo)
-
-for i in range(4*5):
-    t90(arlo)
-
+for i in range(5):
+    sq(arlo,meter)
+#sq(arlo,meter)
+#line(arlo,meter*4)
