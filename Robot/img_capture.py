afad9761d9f9d15adb1136a506928024783f61aa
ArloUno <pi@arlouno>
2024-09-11 15:37:48 +0200

    img

--- Robot/img_capture.py
+++ Robot/img_capture.py
@@ -1,16 +1,23 @@
-
+import time
 try:
-    import picamera2
+    from picamera2 import Picamera2, Preview
     print("Camera.py: Using picamera2 module")
 except ImportError:
     print("Camera.py: picamera2 module not available")
     exit(-1)
+
+cam = Picamera2()
+camera_config = cam.create_preview_configuration()
+cam.configure(camera_config)
+cam.start_preview(Preview.QTGL)
+cam.start()
+time.sleep(2)
 
 while True:
     name = input("Enter the image name (q to quit): ")
     if name == "q":
         break
 
-    picamera2.capture("./img/" + name + ".jpg")
+    cam.capture_file("./img/" + name + ".jpg")
     
     
