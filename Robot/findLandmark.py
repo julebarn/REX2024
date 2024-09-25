f77d437899928dbddceeef14b7d774b7c1d00062
ArloTre (the better Arlo) <pi@arlotr>
2024-09-25 14:20:01 +0200

    Init resources once

--- Robot/findLandmark.py
+++ Robot/findLandmark.py
@@ -3,6 +3,7 @@ import numpy
 from robot import Robot 
 from functools import partial
 import time
+from calibration import calibration
 try:
     from picamera2 import Picamera2, Preview
     print("Camera.py: Using picamera2 module")
@@ -14,7 +15,7 @@ except ImportError:
 aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
 parameters = cv2.aruco.DetectorParameters_create()
 
-bias = -0.043750000000000004
+bias  = calibration["bias"]
 speed = 100
 
 def goLine():
@@ -42,27 +43,20 @@ def foundLandmark(cam=None):
     print(ids)
     return ids != None
 
-def main():
-    print("main")
+try:
+    arlo
+except NameError:
     arlo = Robot()
-    
-    cam = Picamera2()
-    cam.video_configuration.controls.FrameRate = 5.0
-    # camera_config = cam.create_preview_configuration()
-    # cam.configure(camera_config)
-    # cam.start_preview(Preview.QTGL)
-    cam.start()
-    
-    print("Out")
-    #img = cam.capture_array()
-    #img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    #cv2.imshow('Detected Markers', img)
 
-    rotateUntil(partial(foundLandmark, cam), agent=arlo, clockwise=False)
-    while partial(foundLandmark, cam):
-        arlo.go_diff(speed * (1+bias),speed * (1-bias), 1, 1)
-        time.sleep(0.2)
-        arlo.stop()
-    
-if __name__ == "__main__":
-    main()
+try:
+    cam
+except NameError:
+    cam = Picamera2()
+    cam.start()
+
+rotateUntil(partial(foundLandmark, cam), agent=arlo, clockwise=False)
+
+#while partial(foundLandmark, cam):
+#    arlo.go_diff(speed * (1+bias),speed * (1-bias), 1, 1)
+#    time.sleep(0.2)
+#    arlo.stop()
