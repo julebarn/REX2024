cb82458d45d60699fcf509f63fb75280201139f6
MostafaElshamy <mostafaelshamy@aucegypt.edu>
2024-10-07 13:41:28 +0200

    save path to file

--- Robot/pathFinding.py
+++ Robot/pathFinding.py
@@ -85,32 +85,36 @@ rrt.plot_initial()
 path = rrt.find_path()
 rrt.plot_final(path)
 
-def goLine(dist):
-    print("Going", dist, "meters")
-    arlo.go_diff(speed * (1+bias),speed * (1-bias), 1, 1)
-    time.sleep(calibration["speed"]*dist)
-    arlo.stop()
-    time.sleep(0.5)
+output_file = open("path.csv",'w')
+output_file.write(path)
+output_file.close
 
-def rotateDeg(deg, speed=60, clockwise=False):
-    print("Rotating", deg, "degrees", ("clockwise" if clockwise else "countercloskwise"))
-    if deg < 5:
-        return
+# def goLine(dist):
+#     print("Going", dist, "meters")
+#     arlo.go_diff(speed * (1+bias),speed * (1-bias), 1, 1)
+#     time.sleep(calibration["speed"]*dist)
+#     arlo.stop()
+#     time.sleep(0.5)
+
+# def rotateDeg(deg, speed=60, clockwise=False):
+#     print("Rotating", deg, "degrees", ("clockwise" if clockwise else "countercloskwise"))
+#     if deg < 5:
+#         return
     
-    arlo.go_diff(speed * (1+bias),speed * (1-bias), 
-                0 if clockwise else 1,
-                1 if clockwise else 0)
-    time.sleep(deg/calibration["rotation_speed"])
-    arlo.stop()
-    time.sleep(0.5)
+#     arlo.go_diff(speed * (1+bias),speed * (1-bias), 
+#                 0 if clockwise else 1,
+#                 1 if clockwise else 0)
+#     time.sleep(deg/calibration["rotation_speed"])
+#     arlo.stop()
+#     time.sleep(0.5)
 
-for _, turn, dist in accumulate(path, movement, initial=((0,0),0,0)):
-    clockwise = False
-    if turn<0:
-        clockwise = True
-        turn = -turn 
-    rotateDeg(turn,60,clockwise)
-    goLine(dist)
+# for _, turn, dist in accumulate(path, movement, initial=((0,0),0,0)):
+#     clockwise = False
+#     if turn<0:
+#         clockwise = True
+#         turn = -turn 
+#     rotateDeg(turn,60,clockwise)
+#     goLine(dist)
 
 
 
