5d51db0a8e93db143f13626d6227c280173e3afc
MostafaElshamy <mostafaelshamy@aucegypt.edu>
2024-10-07 14:04:38 +0200

    fixed path

--- Robot/pathFinding.py
+++ Robot/pathFinding.py
@@ -78,19 +78,20 @@ start = (0, 0)
 goal = (0, 1.8)
 map_size = (1.8, 3)
 
-#rrt = RRT(start, goal, map_size, obstacles, step_size=0.5, max_iter=1000)
+rrt = RRT(start, goal, map_size, obstacles, step_size=0.5, max_iter=1000)
 
-#rrt.plot_initial()
+rrt.plot_initial()
 #path = rrt.find_path()
-#rrt.plot_final(path)
-path = [[0, 0],
-[0.27558484319520465, 0.41719658939290777],
+path = [[0, 0],[0.27558484319520465, 0.41719658939290777],
 [0.7364580606932789, 0.611093153036962],
 [0.7549048590666272, 1.1107527527944099],
 [0.957324827579686, 1.567946537033842],
 [0.668544966572807, 1.9761207545891006],
 [0.17724971919484672, 2.0690130573048195],
 [0, 1.8]]
+rrt.plot_final(path)
+
+
 
 output_file = open("path.csv",'w')
 for node in path:
