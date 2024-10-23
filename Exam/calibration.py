b80aa42d49c1e83abfbef8c182b8a970aa6327ad
ArloTre (the better Arlo) <pi@arlotr>
2024-10-23 14:05:03 +0200

    calibrated for speed 60

--- Exam/calibration.py
+++ Exam/calibration.py
@@ -3,7 +3,7 @@ calibration = {
     # deg per second @ 60
     "bias": -0.03125000000000001,
     # 100 * (1+bias),100 * (1-bias)
-    # Straight line at 100
-    "speed": 1.5
+    # Straight line at 60
+    "speed": 2.6
     # seconds/meter
 }
