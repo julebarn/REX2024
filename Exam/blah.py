33c2d3a80329034f156a500761c049c0eee1b48b
MostafaElshamy <mostafaelshamy@aucegypt.edu>
2024-10-25 17:02:13 +0200

    delete nothing

--- Exam/blah.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def angleBetweenPoints(a,b):
-    return(math.degrees(math.atan((a[0]-b[0])/(a[1]-b[1]))))
-
-a = (0,0)
-b = (0.27558484319520465, 0.41719658939290777)
-print(angleBetweenPoints(a,b))
